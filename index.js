b0f65cf1ec75ea59bf6d424f8cbadac5f9d55f15
shanreed <srtech5set@gmail.com>
2020-01-03 03:05:48 -0800

    added starter code

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const server = require('./api/server.js');
+
+
+
+
+server.get('/', (req, res) => {
+    res.send('HELLO, YOU HAVE ENTERED MY SERVER')
+})
+
+const port =process.env.PORT;
+server.listen(port, () => console.log(`\n*** Server Running on http://localhost:${port} ***\n`))
