b39b821baff39cd9ef003e3c5d4598508806a9d4
shanreed <srtech5set@gmail.com>
2020-01-03 03:28:47 -0800

    added environment variables

--- api/server.js
+++ api/server.js
@@ -6,4 +6,15 @@ const helmet = require('helmet');
 const server = express();
 server.use(helmet())
 
+server.get('/', async (req, res) => {
+    try {
+      const shoutouts = "SHOUTS";
+    //   const messageOfTheDay = process.env.MOTD || 'Hello World!'; // add this line
+      res.status(200).json({ motd: process.env.MOTD, shoutouts }); // change this line
+    } catch (error) {
+      console.error('\nERROR', error);
+      res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+    }
+  });
+
 module.exports = server;
--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const server = require('./api/server.js');
 
 
 
-server.get('/', (req, res) => {
-    res.send('HELLO, YOU HAVE ENTERED MY SERVER')
-})
+// server.get('/', (req, res) => {
+//     res.send('HELLO, YOU HAVE ENTERED MY SERVER')
+// })
 
 const port =process.env.PORT;
 server.listen(port, () => console.log(`\n*** Server Running on http://localhost:${port} ***\n`))
